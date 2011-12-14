029ad577bca7840b1f02e89c781f63ea43b255e2
Bhaskar Nidumukalla <bhaskar@denaglobal.com>
2011-12-13 19:40:06 -0800

    adding the problem description in the head of the scripts

--- problem_3.rb
+++ problem_3.rb
@@ -1,3 +1,6 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
 def is_prime?(number)
   (2..Math.sqrt(number).floor).none? do |x|
     number % x == 0
