5d97de02baaa9701223677a135586d10db0ccb89
Bhaskar Nidumukalla <bhaskar.nidumukalla@dena.jp>
2016-02-19 20:42:57 +0900

    skeleton code

--- go/src/problem_45/problem_45.go
+++ go/src/problem_45/problem_45.go
@@ -1,1 +1,18 @@
 package problem_45
+
+import (
+	"common"
+)
+
+func TriangularAndPentagonalAndHexagonalNumbers(n uint64) {
+	t_seq = common.TriangularNumberSeq()
+	p_seq = common.PentagonalNumberSeq()
+	h_seq = common.HexagonalNumberSeq()
+
+	var intersection uint64
+	select {
+	case tn := <-t_seq:
+	case pn := <-p_seq:
+	case hn := <-h_seq:
+	}
+}
